df70f92fedf42afdfe189299cad07aa76ef544fe
Bohdan Katsevych <94533356+bkatsevych@users.noreply.github.com>
2024-08-19 18:12:06 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()],
-  site: "https://bkatsevych.github.io",
-  base: "/collaps-website"
+    integrations: [tailwind(), sitemap()],
+    site: "https://bkatsevych.github.io",
+    base: "/collaps-website",
 });
